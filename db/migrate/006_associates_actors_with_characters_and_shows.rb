4722ef2aecf81814f620baaa0e3a2bb44fd12496
Chris Cardle <cdcardle@gmail.com>
2018-02-08 16:26:07 +0000

    automatically backed up by learn

--- db/migrate/006_associates_actors_with_characters_and_shows.rb
+++ db/migrate/006_associates_actors_with_characters_and_shows.rb
@@ -1,5 +1,6 @@
 class AssociatesActorsWithCharactersAndShows < ActiveRecord::Migration
   def change
-    add_column :shows, :actor_id, :integer
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
   end
 end
